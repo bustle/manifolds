7c1905fef8ca26223af69f8ff2a5beee224a53b1
claytongentry <clayton.gentry@bustle.com>
2024-04-30 16:18:52 -0400

    Add project

--- lib/manifolds/cli.rb
+++ lib/manifolds/cli.rb
@@ -2,22 +2,41 @@
 
 require "thor"
 require "fileutils"
+require "logger"
 
 module Manifolds
   # CLI provides command line interface functionality
   # for creating and managing umbrella projects for data management.
   class CLI < Thor
-    desc "new_umbrella NAME", "Generate a new umbrella project for data management"
-    def new_umbrella(name)
+    def initialize(*args)
+      super
+      @logger = Logger.new($stdout)
+      @logger.level = Logger::INFO
+    end
+
+    desc "init NAME", "Generate a new umbrella project for data management"
+    def init(name)
       directory_path = "./#{name}/projects"
       FileUtils.mkdir_p(directory_path)
-      puts "Created umbrella project '#{name}' with a projects directory."
+      @logger.info "Created umbrella project '#{name}' with a projects directory."
       # Generate Gemfile inside the new umbrella project
       File.open("./#{name}/Gemfile", "w") do |file|
         file.puts "source 'https://rubygems.org'"
         file.puts "gem 'thor'"
       end
-      puts "Generated Gemfile for the umbrella project."
+      @logger.info "Generated Gemfile for the umbrella project."
+    end
+
+    desc "add PROJECT_NAME", "Add a new project within the current umbrella project"
+    def add(project_name)
+      unless Dir.exist?("./projects")
+        @logger.error("Not inside a Manifolds umbrella project.")
+        return
+      end
+
+      FileUtils.mkdir_p("tables")
+      FileUtils.mkdir_p("routines")
+      @logger.info "Added project '#{project_name}' with tables and routines directories."
     end
   end
 end
--- spec/manifolds/cli_spec.rb
+++ spec/manifolds/cli_spec.rb
@@ -4,16 +4,28 @@ require_relative "../../lib/manifolds/cli"
 require "fileutils"
 
 RSpec.describe Manifolds::CLI do
-  describe "#new_umbrella" do
-    subject(:cli) { described_class.new } # Using a named subject
+  let(:project_name) { "commerce" }
+  let(:sub_project_name) { "Pages" }
 
-    let(:project_name) { "test_project" }
+  before do
+    # Redirect stdout to null device to silence output
+    @original_stdout = $stdout
+    $stdout = File.open(File::NULL, "w")
+  end
+
+  after do
+    # Restore stdout
+    $stdout = @original_stdout
+  end
+
+  describe "#init" do
+    subject(:cli) { described_class.new } # Using a named subject
 
     before do
       allow($stdout).to receive(:write)
       allow(FileUtils).to receive(:mkdir_p)
       allow(File).to receive(:open)
-      cli.new_umbrella(project_name) # Using the named subject
+      cli.init(project_name) # Using the named subject
     end
 
     after do
@@ -26,6 +38,32 @@ RSpec.describe Manifolds::CLI do
 
     it "creates the Gemfile" do
       expect(File).to have_received(:open).with("./#{project_name}/Gemfile", "w")
+    end
+  end
+
+  describe "#add" do
+    context "within an umbrella project" do
+      before do
+        FileUtils.mkdir_p("commerce/projects") # Simulate an umbrella project
+        Dir.chdir("commerce")
+      end
+
+      after do
+        Dir.chdir("..")
+        FileUtils.rm_rf("commerce")
+      end
+
+      it "creates directories" do
+        expect { described_class.new.add("Pages") }.to output(/Added project 'Pages'/).to_stdout
+        expect(Dir.exist?("tables")).to be true
+        expect(Dir.exist?("routines")).to be true
+      end
+    end
+
+    context "outside an umbrella project" do
+      it "does not allow adding projects" do
+        expect { described_class.new.add("Pages") }.to output(/(.*)Not inside a Manifolds umbrella project./).to_stdout
+      end
     end
   end
 end
