08aca684b673cbb9327c48eb022521f8a08def13
claytongentry <cg3ntry@gmail.com>
2024-11-04 11:21:16 -0500

    lax

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -24,8 +24,8 @@ SimpleCov.start do
   add_group "Services", "lib/manifolds/services"
 
   # Set the minimum coverage percentage
-  minimum_coverage 100
-  minimum_coverage_by_file 100
+  minimum_coverage 95
+  minimum_coverage_by_file 90
 
   # Fail the build if coverage drops below threshold
   refuse_coverage_drop
