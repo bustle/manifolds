5e460f8fb1bbdadec583e67119997e22142e6859
claytongentry <cg3ntry@gmail.com>
2024-11-08 16:08:04 -0500

    Add spec persistence

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -44,6 +44,8 @@ RSpec.configure do |config|
     mocks.verify_partial_doubles = true
   end
 
+  config.example_status_persistence_file_path = "spec/examples.txt"
+
   config.shared_context_metadata_behavior = :apply_to_host_groups
   config.order = :random
 end
